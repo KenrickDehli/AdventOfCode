dda7f3a4183d80ddd7a38b143084955b12589982
Dehli Kenrick <Kenrick.Dehli@de.bosch.com>
2024-12-04 14:40:16 +0100

    progress

--- 2024/day_4/dayFour.go
+++ 2024/day_4/dayFour.go
@@ -24,7 +24,7 @@ func main() {
 			substrings := buildString(input, i, j, countOfRows, len(input[i]))
 			for k := range substrings {
 				if strings.Contains(substrings[k], validForward) || strings.Contains(substrings[k], validBackward) {
-					fmt.Println(substrings[k])
+					fmt.Println(i, substrings[k])
 					countOfXMAS++
 				}
 			}
@@ -53,14 +53,14 @@ func buildString(str []string, rowIndex int, columnIndex int, countOfRows int, c
 	right = string(str[rowIndex][columnIndex:calcBounds(columnIndex+4, countOfColumns-1, false)])
 	left = string(str[rowIndex][calcBounds(columnIndex-4, 0, true):columnIndex])
 
-	up = string(str[calcBounds(rowIndex-1, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-2, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-3, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-4, 0, true)][columnIndex])
-	down = string(str[calcBounds(rowIndex+1, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+4, countOfRows, false)][columnIndex])
+	up = string(str[calcBounds(rowIndex, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-1, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-2, 0, true)][columnIndex]) + string(str[calcBounds(rowIndex-3, 0, true)][columnIndex])
+	down = string(str[calcBounds(rowIndex, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+1, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][columnIndex]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][columnIndex])
 
-	upright = string(str[calcBounds(rowIndex-1, 0, true)][calcBounds(columnIndex+1, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-2, 0, true)][calcBounds(columnIndex+2, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-3, 0, true)][calcBounds(columnIndex+3, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-4, 0, true)][calcBounds(columnIndex+4, countOfColumns-1, false)])
-	upleft = string(str[calcBounds(rowIndex-1, 0, true)][calcBounds(columnIndex-1, 0, true)]) + string(str[calcBounds(rowIndex-2, 0, true)][calcBounds(columnIndex-2, 0, true)]) + string(str[calcBounds(rowIndex-3, 0, true)][calcBounds(columnIndex-3, 0, true)]) + string(str[calcBounds(rowIndex-4, 0, true)][calcBounds(columnIndex-4, 0, true)])
+	upright = string(str[calcBounds(rowIndex, 0, true)][calcBounds(columnIndex, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-1, 0, true)][calcBounds(columnIndex+1, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-2, 0, true)][calcBounds(columnIndex+2, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex-3, 0, true)][calcBounds(columnIndex+3, countOfColumns-1, false)])
+	upleft = string(str[calcBounds(rowIndex, 0, true)][calcBounds(columnIndex, 0, true)]) + string(str[calcBounds(rowIndex-1, 0, true)][calcBounds(columnIndex-1, 0, true)]) + string(str[calcBounds(rowIndex-2, 0, true)][calcBounds(columnIndex-2, 0, true)]) + string(str[calcBounds(rowIndex-3, 0, true)][calcBounds(columnIndex-3, 0, true)])
 
-	lowleft = string(str[calcBounds(rowIndex+1, countOfRows, false)][calcBounds(columnIndex-1, 0, true)]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][calcBounds(columnIndex-2, 0, true)]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][calcBounds(columnIndex-3, 0, true)]) + string(str[calcBounds(rowIndex+4, countOfRows, false)][calcBounds(columnIndex-4, 0, true)])
-	lowright = string(str[calcBounds(rowIndex+1, countOfRows, false)][calcBounds(columnIndex+1, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][calcBounds(columnIndex+2, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][calcBounds(columnIndex+3, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+4, countOfRows, false)][calcBounds(columnIndex+4, countOfColumns-1, false)])
+	lowleft = string(str[calcBounds(rowIndex, countOfRows, false)][calcBounds(columnIndex, 0, true)]) + string(str[calcBounds(rowIndex+1, countOfRows, false)][calcBounds(columnIndex-1, 0, true)]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][calcBounds(columnIndex-2, 0, true)]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][calcBounds(columnIndex-3, 0, true)])
+	lowright = string(str[calcBounds(rowIndex, countOfRows, false)][calcBounds(columnIndex, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+1, countOfRows, false)][calcBounds(columnIndex+1, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+2, countOfRows, false)][calcBounds(columnIndex+2, countOfColumns-1, false)]) + string(str[calcBounds(rowIndex+3, countOfRows, false)][calcBounds(columnIndex+3, countOfColumns-1, false)])
 
 	return []string{right, left, up, down, upright, upleft, lowleft, lowright}
 }
